9335eeee5cb261227bea0772e48c9e6c033eace9
jai mishra <jaidev7823@gmail.com>
2026-05-15 21:57:05 +0530

    made graph smooth

--- backend/app/services/feature_service.py
+++ backend/app/services/feature_service.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.signal import medfilt
 
 from app.core.config import TARGET_SR
-
+STEP = 3
 
 def extract_pause_regions(pause_mask, times):
     regions = []
@@ -19,27 +19,59 @@ def extract_pause_regions(pause_mask, times):
         else:
             i += 1
     return regions
+def smooth_signal(signal, window_size=15):
+
+    kernel = np.ones(window_size) / window_size
+
+    return np.convolve(
+        signal,
+        kernel,
+        mode="same"
+    )
 
 
 def extract_features(audio_path):
     y, sr = librosa.load(audio_path, sr=TARGET_SR)
 
     hop_length = 256
-
-    rms = librosa.feature.rms(y=y, hop_length=hop_length)[0]
+    rms = librosa.feature.rms(
+        y=y,
+        hop_length=hop_length
+    )[0]
+        
+    rms = smooth_signal(rms, window_size=25)
 
     pitch = librosa.yin(y, fmin=50, fmax=300, sr=sr, hop_length=hop_length)
     pitch = np.nan_to_num(pitch, nan=0.0)
-    pitch = medfilt(pitch, kernel_size=11)
+    pitch = medfilt(pitch, kernel_size=21)
+    
+    pitch = smooth_signal(
+        pitch,
+        window_size=15
+    )
 
     times = librosa.frames_to_time(np.arange(len(rms)), sr=sr, hop_length=hop_length)
 
     pause_mask = rms < np.percentile(rms, 15)
 
     delta_rms = np.abs(np.diff(rms, prepend=rms[0]))
-    cps = np.convolve(delta_rms, np.ones(20) / 20, mode="same")
+    cps = np.convolve(
+        delta_rms,
+        np.ones(20) / 20,
+        mode="same"
+    )
+    
+    cps = smooth_signal(
+        cps,
+        window_size=20
+    )
 
     pause_regions = extract_pause_regions(pause_mask, times)
+    times = times[::STEP]
+    rms = rms[::STEP]
+    pitch = pitch[::STEP]
+    cps = cps[::STEP]
+    pause_mask = pause_mask[::STEP]
 
     return {
         "rms": rms.tolist(),
